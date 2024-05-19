5587df252fc6f9758500234b3b2187bbe1e29bfe
Alessandro Bernardello <53372753+alessandroberna@users.noreply.github.com>
2024-05-19 17:17:29 +0200

    Creato progetto; closes #1

---
+++ src/test/java/it/unipd/mtss/AppTest.java
@@ -0,0 +1,20 @@
+package it.unipd.mtss;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
